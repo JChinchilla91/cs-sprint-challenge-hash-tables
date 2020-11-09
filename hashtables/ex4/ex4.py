2591023fc253ad7897a219bc4125d67084fe5980
Joshua Chinchilla <JoshuaChinchilla91@gmail.com>
2020-11-08 19:11:59 -0800

    completed four problems

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,6 +4,15 @@ def has_negatives(a):
     """
     # Your code here
 
+    negatives = {}
+    result = []
+
+    for num in a:
+        if num < 0:
+            negatives[-num] = num
+
+    result = [num for num in a if num in negatives]
+
     return result
 
 
